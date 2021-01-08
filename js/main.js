fed70d3806e1cb0ef90fbf78d4a58f1c0c49a2ab
Toricheli <t0637242529@gmail.com>
2021-01-08 20:42:43 +0200

    add tokens

--- js/main.js
+++ js/main.js
@@ -71,11 +71,11 @@ const sendForm = () => {
         // generate a five digit number for the contact_number variable
         this.contact_number.value = Math.random() * 100000 | 0;
         // these IDs from the previous steps
-        emailjs.sendForm('contact_service', 'contact_form', this)
+        emailjs.sendForm('service_p87kxia', 'template_s5whvsc', this)
             .then(function() {
                 console.log('SUCCESS!');
             }, function(error) {
-                console.log('FAILED...', error);
+                console.error('FAILED...', error);
             });
     });
 }
