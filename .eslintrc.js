1165ba13529c33e6c9d2d2c7888721f6854ca3d8
IvanNavin <e0637242529@gmail.com>
2021-08-30 07:53:03 +0300

    pause adding webpack

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'max-len': ['error', { code: 120, comments: 140 }],
+    'no-bitwise': "off",
+    "no-return-assign": "off",
+    "no-plusplus": "off",
+    "no-param-reassign": "off",
+    "class-methods-use-this": "off",
+    "no-unused-expressions": "off",
+  },
+};
