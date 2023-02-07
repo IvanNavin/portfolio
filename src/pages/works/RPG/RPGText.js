2ab8852535feec01abff58b63ee700a71d411b11
IvanNavin777 <iholovko@leviathansecurity.com>
2023-02-07 09:17:11 +0200

    update RPGText.js

--- src/pages/works/RPG/RPGText.js
+++ src/pages/works/RPG/RPGText.js
@@ -5,21 +5,24 @@ const ru = {
         Прикольный опыт работы с сокетами и с серверной частью.
         Есть онлайн чат.
         Стек: HTML / SASS / JS/ Webpack5 / Websocket(socket.io) / EsLint / Prettier
-        Что бы проверить работоспособность игры и чата, можно запустить игру на нескольких вкладках.`,
+        Что бы проверить работоспособность игры и чата, можно запустить игру на нескольких вкладках.
+        К сожалению бекенд сейчас не работает.`,
 };
 const ua = {
   text: `Тут вдалося написати онлайн гру в якій можуть грати відразу кілька людей.
          Прикольний досвід роботи з сокетами та серверною частиною.
          Є онлайн-чат.
          Стек: HTML / SASS / JS / Webpack5 / Websocket (socket.io) / EsLint / Prettier
-         Щоб перевірити працездатність гри та чату, можна запустити гру на декількох вкладках.`,
+         Щоб перевірити працездатність гри та чату, можна запустити гру на декількох вкладках.
+         Нажаль зараз бекенд не працює.`,
 };
 const en = {
   text: `Here I managed to write an online game in which several people can play at once.
          Cool experience with sockets and server side.
          There is an online chat.
          Stack: HTML / SASS / JS / Webpack5 / Websocket (socket.io) / EsLint / Prettier
-         To check the functionality of the game and chat, you can launch the game in several tabs.`,
+         To check the functionality of the game and chat, you can launch the game in several tabs.
+         Unfortunately, the backend is not working now.`,
 };
 const RPGText = { ru, ua, en };
 
