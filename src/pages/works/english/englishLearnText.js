69d0a7b0b1fad0d4bab7e636496a336199e7f279
IvanNavin777 <iholovko@leviathansecurity.com>
2023-02-17 11:38:47 +0200

    update update text

--- src/pages/works/english/englishLearnText.js
+++ src/pages/works/english/englishLearnText.js
@@ -1,7 +1,7 @@
 import { btn } from '../btn';
 
 const ru = {
-  text: `English learn - это результат Реакт-марафона от Зара Захарова
+  text: `English learning - это результат Реакт-марафона от Зара Захарова.</br>
         В ходе этого марафона мы познакомились с React и его жизненными циклами, научились работать с формами и с сервером "Firebase". Научились работать с пропсами и стейтом, пробрасывали их через компоненты и обратно. Для начала написали самописный redux, что бы понимать как он работает и в последствии подключили его. Иконки и формы мы брали из коллекции AntDesign. Подключили апишку словаря и использовали хранилище firebase для запоминания слов юзера. Прикрутили React Router и Thunk.
         Что бы использовать возможности сайта, нужно внести неизвестное английское слово в поле ввода. Далее это слово появится в базе данных "Firebase" и под полем ввода. Если кликнуть на слово, карточка перевернётся и с другой стороны карточки будет уже слово на русском языке. Далее это слово можно вычеркнуть, как запомненное или удалить как ненужное. Тогда это слово удалится и из базы ваших слов.
         </br>
@@ -12,20 +12,20 @@ const ru = {
         </br>`,
 };
 const ua = {
-  text: `English learn – це результат Реакт-марафону від Зара Захарова
+  text: `English learning – це результат Реакт-марафону від Зара Захарова.</br>
         У ході цього марафону ми познайомилися з React та його життєвими циклами, навчилися працювати з формами та сервером "Firebase". Навчилися працювати з пропсами та стейтом, прокидали їх через компоненти та назад. Для початку написали самописний redux, щоб розуміти як він працює і згодом підключили його. Іконки та форми ми брали із колекції AntDesign. Підключили апішку словника і використовували сховище firebase для запам'ятовування слів користувача. Прикрутили React Router та Thunk.
-        Щоб використати можливості сайту, потрібно внести невідоме англійське слово у поле введення. Далі це слово з'явиться в базі даних Firebase і під полем введення. Якщо натиснути на слово, картка перевернеться і з іншого боку картки буде вже слово російською мовою. Далі це слово можна викреслити як запам'ятоване або видалити як непотрібне. Тоді це слово видалиться і з бази ваших слів.
+        Щоб використати можливості сайту, потрібно внести невідоме англійське слово у поле введення. Далі це слово з'явиться в базі даних Firebase і під полем введення. Якщо натиснути на слово, картка перевернеться і з іншого боку картки буде вже слово російською мовою. Далі це слово можна викреслити як запам'ятоване або видалити як непотрібне. Тоді це слово видалиться із бази ваших слів.
         </br>
         Логин: reactmarathon@gmail.com
         Пароль: 12345678
         </br>`,
 };
 const en = {
-  text: `English learn is the result of the React Marathon from Zar Zakharov
-        During this marathon, we got acquainted with React and its life cycles, learned how to work with forms and with the Firebase server. We learned how to work with props and states, passed them through the components and back. To begin with, we wrote a self-written redux to understand how it works and later connected it. We took icons and shapes from the AntDesign collection. We connected the dictionary apish and used the firebase storage to memorize the user's words. Attached React Router and Thunk.
-        To use the site's capabilities, you need to enter an unknown English word in the input field. Further, this word will appear in the "Firebase" database and under the input field. If you click on a word, the card will turn over and on the other side of the card there will already be a word in Russian. Further, this word can be crossed out as memorized or deleted as unnecessary. Then this word will be deleted from the base of your words.
+  text: `English learning is the result of the React Marathon from Zar Zakharov.</br>
+        During this marathon we got acquainted with React and its life cycles, learned how to work with forms and with the Firebase server. From the marathon I got the skill how to work with props and states, and passed them through the components and back. To begin with, we wrote a self-written redux to understand how it works and later connected it. We took icons and shapes from the AntDesign collection. We connected the dictionary api and used the firebase storage to memorize user's words. Then we Attached React Router and Thunk.
+        To use the site's capabilities, you need to enter an unknown English word in the input field. Further, this word will appear in the "Firebase" database and under the input field. If you click on a word, the card will flip and on the other side you can find the translation. This word can be crossed out as memorized or deleted as unnecessary and consequently removed from the base of your words.
         </br>
-        Login: reactmarathon@gmail.com
+        Login: reactmarathon@gmail.com</br>
         Password: 12345678
         </br>`,
 };
--- src/pages/works/thanos/thanosEffectText.js
+++ src/pages/works/thanos/thanosEffectText.js
@@ -6,7 +6,7 @@ const ru = {
         Нужно сделать распад картинок, как в фильме Мстители 3: Война бесконечности. Картинки должны распадаться на пепел и разлетаться по ветру.
         Немного полапатив интернет, я обнаружил много примеров данной реализации.
         Все примеры которые я находил, были сделаны с библиотекой html2canvas, меня это немного не устраивало! Т.к. мне нужно разщиплять на атомы только картинки а не любой контент. А для этих целей подойдут и обычные средства canvas.
-        Поэтому я сделал этот эффект без применения библиотеки html2canvas`,
+        Поэтому я сделал этот эффект без применения библиотеки html2canvas.`,
 };
 const ua = {
   text: `Працюючи в EvoPlay мені надійшло завдання: відтворити ефект Таноса.
@@ -14,15 +14,15 @@ const ua = {
         Потрібно зробити розпад картинок, як у фільмі «Месники 3». Картинки повинні розпадатися на попіл і розлітатися за вітром.
         Трохи полапатив інтернет, я виявив багато прикладів даної реалізації.
         Всі приклади, які я знаходив, були зроблені з бібліотекою html2canvas, мене це трохи не влаштовувало! Т.к. мені потрібно розщиплювати на атоми тільки картинки, а не будь-який контент. А для цих цілей підійдуть і звичайні засоби canvas.
-        Тому я зробив цей ефект без застосування бібліотеки html2canvas`,
+        Тому я зробив цей ефект без застосування бібліотеки html2canvas.`,
 };
 const en = {
-  text: `While working in EvoPlay, I received a task: to reproduce the Thanos effect.
+  text: `While working in EvoPlay, I received a task to reproduce the Thanos effect.
         What should be done?
-        You need to make the disintegration of the pictures, as in the movie Avengers 3 Pictures should disintegrate into ash and fly in the wind.
-        After a little surfing the Internet, I found many examples of this implementation.
-        All the examples that I found were made with the html2canvas library, it didn't suit me a bit! Because I need to split into atoms only pictures and not any content. Ordinary canvas is fine for this.
-        So I made this effect without using html2canvas library`,
+        The main purpose was to make the disintegration of the pictures likes in the movie Avengers 3. The pictures should disintegrate into ash and fly in the wind.
+        After surfing on the Internet, I found many examples of this implementation.
+        For I needed to split only pictures in atoms but not the content, I found these examples unsuitable for this purpose.
+        However, ordinary canvas is much better for it, so I made this effect without using html2canvas library.`,
 };
 const RPGText = { ru, ua, en };
 
